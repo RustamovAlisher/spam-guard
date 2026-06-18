3a63fc48fd5365e8fb71c18ed0931028892b75ca
Alisher <alisherrustamov32@gmail.com>
2026-06-18 21:04:14 +0500

    commit-spam bot  struktura yaratildi

---
+++ src/main/java/uz/antispam/SpamGuardApplication.java
@@ -0,0 +1,13 @@
+package uz.antispam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpamGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpamGuardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/antispam/SpamGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.antispam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpamGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
